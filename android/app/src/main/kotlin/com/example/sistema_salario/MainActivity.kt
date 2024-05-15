6575211b67ee02b5ae6c97e46c74cc406f523922
leonardomiclos <leonardo.miclos@insumosholambra.com.br>
2024-05-15 11:38:14 -0300

    Trabalho maromo

---
+++ android/app/src/main/kotlin/com/example/sistema_salario/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sistema_salario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'salario_page.dart';
+
+void main() {
+  runApp(const AppSalario());
+}
+
+class AppSalario extends StatelessWidget {
+  const AppSalario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Salário (App)',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SalarioHome(title: 'Cálculo de Salário'),
+    );
+  }
+}
